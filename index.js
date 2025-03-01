6496d9c6aa7edf768611d981fa668978fe7e24f0
mhsumon0174 <mhsumon0174@gmail.com>
2025-03-01 15:10:19 +0600

    modified

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ for(const button of buttons){
 
         const pendingTask=parseInt(document.getElementById('pending-task').innerText);
         document.getElementById('pending-task').innerText=pendingTask-1;
-        
+        if(button===buttons[buttons.length-1]){
+            alert('You Have Completed All The Task Successfully')
+        }
     })
 }
 
